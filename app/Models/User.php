2a831fa54596e316b043c02a8ef511205af346ac
Antonio de jesus cruz guerrero <antonio.cruzg@gmodelo.com.mx>
2023-07-13 12:16:44 -0600

    init

---
+++ app/Models/User.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class User extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
+
+    public function statuses()
+    {
+        return $this->hasMany(Status::class)->orderBy('order');
+    }
+    protected static function booted()
+    {
+        static::created(function ($user) {
+            // Create default statuses
+            $user->statuses()->createMany([
+                [
+                    'title' => 'Backlog',
+                    'slug' => 'backlog',
+                    'order' => 1
+                ],
+                [
+                    'title' => 'Up Next',
+                    'slug' => 'up-next',
+                    'order' => 2
+                ],
+                [
+                    'title' => 'In Progress',
+                    'slug' => 'in-progress',
+                    'order' => 3
+                ],
+                [
+                    'title' => 'Done',
+                    'slug' => 'done',
+                    'order' => 4
+                ]
+            ]);
+        });
+    }
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
+}
